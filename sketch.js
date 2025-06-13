1d83d3d537a1258c1a43ad99cd6cc1b80d30721a
sungyunajo <joon.spkly@gmail.com>
2025-06-13 23:21:40 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1168,7 +1168,7 @@ function ticket() {
   fill(255);
   textAlign(CENTER);
   textFont(font2);
-  text("티켓의 요소 위에 마우스를 올려 해설을 확인하세요", 990, 160);
+  text("티켓의 요소 위에 마우스를 올려 해설을 확인하세요", 1000, 160);
 
   //1. 배경 모션(청각_answer4)
   for (let i = 0; i < answer4.length; i++) {
@@ -1363,7 +1363,7 @@ function ticket() {
   text(name, 404, 387 - 35);
   textFont(font4);
   textSize(20);
-  text(date, 556, 468 - 34);
+  text(date, 556, 468 - 36);
   textAlign(CENTER);
   textFont(font2);
   stroke(255);
