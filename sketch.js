f0babb255b09ea350e7bd364ac610540c957cae0
sungyunajo <joon.spkly@gmail.com>
2025-06-15 16:26:19 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -332,6 +332,7 @@ function notice() {
     text("가능한 구체적으로, 그리고 생생하게 상황을 상상해보시길 당부드립니다.\n점, 선, 면, 그리고 다채로운 색채로 표현되는 감각 세계가 펼쳐질 것입니다.", width/2, height/2);
   } else if(enterKey==4){
     state='question'
+    enterKey = 0;
   }
 }
 
