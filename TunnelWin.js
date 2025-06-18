2dc843fa84bf3a7e82a8213d3c917b42d908ac36
nowhyes <ixnerpeaxe@gmail.com>
2025-06-18 11:27:32 +0900

    Update TunnelWin.js

--- TunnelWin.js
+++ TunnelWin.js
@@ -178,7 +178,7 @@ class TunnelWin {
 
     // 터널 끝나기 전까지 이동하는 원
     if (this.runTime !== null) {
-      let moveDuration = 24000; // 터널 종료 전까지
+      let moveDuration = 19000; // 터널 종료 전까지
       let progress = constrain((millis() - this.runTime) / moveDuration, 0, 1);
 
       let circleX = lerp(741.5, 945.5, progress);
