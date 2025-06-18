9b532568ae4be37f3f95bdfaaf2227d36774a0ff
nowhyes <ixnerpeaxe@gmail.com>
2025-06-18 14:13:07 +0900

    Update Tunnel2.js

--- Tunnel2.js
+++ Tunnel2.js
@@ -159,11 +159,11 @@ class Tunnel2 {
 
 
         // 🌑 디졸브 효과를 배경에 적용
-    if (alpha > 0) {
+    /*if (alpha > 0) {
       fill(0, alpha);
       noStroke();
       rect(0, 0, width, height);
-    }
+    }*/
     
 
     //if (alpha > 0) {
