7b5f406b49ff60cf6e1690d75961b1f3b6cbdd94
nowhyes <ixnerpeaxe@gmail.com>
2025-06-18 11:17:55 +0900

    Update TunnelSum.js

--- TunnelSum.js
+++ TunnelSum.js
@@ -170,11 +170,36 @@ class TunnelSum {
       alpha = constrain(alpha, 0, 255);
 
     }
+    
+    tint(255, 255 - alpha); // 디졸브가 올라오면 이미지가 사라짐
+    imageMode(CORNER);
+    image(summerbarImg, 0, 0);
+    noTint();
 
-    if (alpha > 0) {
-      background(255, alpha)
+    // 터널 끝나기 전까지 이동하는 원
+    if (this.runTime !== null) {
+      let moveDuration = 24000; // 터널 종료 전까지
+      let progress = constrain((millis() - this.runTime) / moveDuration, 0, 1);
+
+      let circleX = lerp(335.5, 538.5, progress);
+      let circleY = 72.5;
+
+      push();
+      translate(circleX, circleY);
+      noStroke();
+      fill(255, 45, 49, 255 - alpha); // 디졸브에 맞춰 투명도 감소
+      ellipseMode(CENTER);
+      ellipse(0, 0, 15, 15); // 반지름 7.5
+      pop();
     }
 
+
+        // 🌑 디졸브 효과를 배경에 적용
+    if (alpha > 0) {
+      fill(0, alpha);
+      noStroke();
+      rect(0, 0, width, height);
+    }
 
 
     if (alpha >= 255 && this.aniTime === null) {
@@ -187,7 +212,5 @@ class TunnelSum {
       stageScene = 'illu';
       
     }
-imageMode(CORNER);
-image(summerbarImg, 0, 0);
   }
 }
