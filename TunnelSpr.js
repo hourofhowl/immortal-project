c4d4c377abbe6eedd71665bdd7bb8afe63b3484f
nowhyes <ixnerpeaxe@gmail.com>
2025-06-18 13:34:24 +0900

    Update TunnelSpr.js

--- TunnelSpr.js
+++ TunnelSpr.js
@@ -206,10 +206,10 @@ class TunnelSpr {
 
 
         // 🌑 디졸브 효과를 배경에 적용
-    if (alpha > 0) {
-      fill(0, alpha);
-      noStroke();
-      rect(0, 0, width, height);
+    //if (alpha > 0) {
+      //fill(0, alpha);
+     // noStroke();
+      //rect(0, 0, width, height);
     }
     
     //if (alpha > 0) {
