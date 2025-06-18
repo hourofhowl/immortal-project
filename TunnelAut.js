b82faf372e2a8062a34b3a8528a3a6ae35328492
nowhyes <ixnerpeaxe@gmail.com>
2025-06-18 11:20:02 +0900

    Update TunnelAut.js

--- TunnelAut.js
+++ TunnelAut.js
@@ -180,10 +180,35 @@ drawGradientBackground() {
 
     }
 
-    if (alpha > 0) {
-      background(255, alpha)
+   tint(255, 255 - alpha); // 디졸브가 올라오면 이미지가 사라짐
+    imageMode(CORNER);
+    image(springbarImg, 0, 0);
+    noTint();
+
+    // 터널 끝나기 전까지 이동하는 원
+    if (this.runTime !== null) {
+      let moveDuration = 24000; // 터널 종료 전까지
+      let progress = constrain((millis() - this.runTime) / moveDuration, 0, 1);
+
+      let circleX = lerp(538.5, 741.5, progress);
+      let circleY = 72.5;
+
+      push();
+      translate(circleX, circleY);
+      noStroke();
+      fill(255, 45, 49, 255 - alpha); // 디졸브에 맞춰 투명도 감소
+      ellipseMode(CENTER);
+      ellipse(0, 0, 15, 15); // 반지름 7.5
+      pop();
     }
 
+
+        // 🌑 디졸브 효과를 배경에 적용
+    if (alpha > 0) {
+      fill(0, alpha);
+      noStroke();
+      rect(0, 0, width, height);
+    }
 
 
     if (alpha >= 255 && this.aniTime === null) {
