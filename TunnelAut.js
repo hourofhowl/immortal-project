c70c06c3ab46b45389c33efe1f153786e834b659
nowhyes <ixnerpeaxe@gmail.com>
2025-06-18 14:12:27 +0900

    Update TunnelAut.js

--- TunnelAut.js
+++ TunnelAut.js
@@ -204,11 +204,11 @@ drawGradientBackground() {
 
 
         // 🌑 디졸브 효과를 배경에 적용
-    if (alpha > 0) {
+    /*if (alpha > 0) {
       fill(0, alpha);
       noStroke();
       rect(0, 0, width, height);
-    }
+    }*/
 
 
     if (alpha >= 255 && this.aniTime === null) {
