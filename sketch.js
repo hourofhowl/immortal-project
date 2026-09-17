34d8892329cba0bd02b66eb9fb3eacd445d7c7cf
hourofhowl <eastun@kakao.com>
2026-09-17 16:33:55 +0900

    supabase fix 260917

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ let geminiCalled = false;
 let save = false;
 let cnv;
 
-const supabase = window.supabase.createClient(
+var supabase = window.supabase.createClient(
   "https://pkjymvbwcbcqmvhatpcs.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBranltdmJ3Y2JjcW12aGF0cGNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk2MjUwNDAsImV4cCI6MjEwNTIwMTA0MH0.NPzGH8Jbt7cWwOywaaUpfcTX9I0ponQJB2cnLsCuhKc"     // 🔁 당신의 API 키로 교체
 )
